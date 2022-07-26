27c9b2f64dc35b633a1b5ed3c1fd9d9ee2bbdfbe
Hessam Imen <hessam.imen@gmail.com>
2022-07-26 18:14:22 +1000

    Done with assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+#Step 1: Setting Up the Database
 class Song
-
-
+#  Step 2: Building attr_accessors from column names
+# The #table_name Method ===================
   def self.table_name
     self.to_s.downcase.pluralize
   end
-
+# The #column_names Method ===================
   def self.column_names
     DB[:conn].results_as_hash = true
 
@@ -20,39 +20,41 @@ class Song
     end
     column_names.compact
   end
-
+# Metaprogramming our attr_accessors ===================
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
-
+# Step 3: Building an abstract #initialize Method ==================
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
+#Step 4: Writing our ORM Methods ================
+def table_name_for_insert
+  self.class.table_name
+end
+  
 
-  def save
+def col_names_for_insert
+  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+end
+
+def values_for_insert
+  values = []
+  self.class.column_names.each do |col_name|
+    values << "'#{send(col_name)}'" unless send(col_name).nil?
+  end
+  values.join(", ")
+end
+
+# The #save Method:
+def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-
-  def table_name_for_insert
-    self.class.table_name
-  end
-
-  def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
-  end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
+# Selecting Records in a Dynamic Manner
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
